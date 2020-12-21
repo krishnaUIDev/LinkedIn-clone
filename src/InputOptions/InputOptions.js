2b805037c259e0d4af5f56699bcc7f1d18db8c8b
krishnaUIDev <krishna.kondoju007@gmail.com>
2020-12-20 22:51:00 -0500

    Added

--- src/InputOptions/InputOptions.js
+++ src/InputOptions/InputOptions.js
@@ -4,7 +4,7 @@ import "./InputOptions.css";
 const InputOptions = ({ color, title, Icon }) => {
   return (
     <div className="inputOptions">
-      <Icon sytle={{ color: color }} />
+      <Icon style={{ color: color }} />
       <h4>{title}</h4>
     </div>
   );
